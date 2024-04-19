79d5163df3173d45fecd6d2816a97201d6253cc4
Joe <111279157+joekts@users.noreply.github.com>
2024-04-19 20:03:47 +0100

    fixed bean error for survey

--- TicketSystem/src/main/java/com/example/workflow/model/Survey.java
+++ TicketSystem/src/main/java/com/example/workflow/model/Survey.java
@@ -1,9 +1,11 @@
 package com.example.workflow.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Survey {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
